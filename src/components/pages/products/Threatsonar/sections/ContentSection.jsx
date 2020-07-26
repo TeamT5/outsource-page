113ce480948b8c4447eaba1b5349ddaa76b21bee
joseph2068 <windflying2068@gmail.com>
2020-07-26 21:20:13 +0800

    fix:調整 banner style 和 rwd

--- src/components/pages/products/Threatsonar/sections/ContentSection.jsx
+++ src/components/pages/products/Threatsonar/sections/ContentSection.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import styles from './ContentSection.module.scss'
 import useTranslation from 'src/scripts/translations/useTranslation'
 
- const functions = ['solution.sonar.intro.context',
-    'solution.sonar.function.ca-title',
-    'solution.sonar.function.ca-context',
-    'solution.sonar.funtion.ir-title',
-    'solution.sonar.funtion.ir-context',
-    'solution.sonar.funtion.mdr-title',
-    'solution.sonar.funtion.mdr-context']
+ const functions = [
+    {articleWrap: {context: 'solution.sonar.intro.context'}},
+    { articleWrapSecond:
+        { title: 'solution.sonar.function.ca-title',
+         context: 'solution.sonar.function.ca-context'}},
+    {
+        articleWrapThird:
+        {title: 'solution.sonar.funtion.ir-title',
+        context: 'solution.sonar.funtion.ir-context'}},
+    {
+    articleWrapFourth: {
+            title: 'solution.sonar.funtion.mdr-title',
+            context: 'solution.sonar.funtion.mdr-context',
+        },
+    },
+    ]
 
  const ContentSection = () => {
   const { t } = useTranslation()
@@ -18,21 +27,33 @@ import useTranslation from 'src/scripts/translations/useTranslation'
       <div className={styles['article-wrap']}>
         <div className={styles['card']}>
           <img src='/images/ThreatSonar/what_is_sonar.svg'/>
+          <div className={styles['content']}>
+            
+          </div>
         </div>
       </div>
       <div className={styles['article-wrap-second']}>
         <div className={styles['card']}>
           <img src='/images/ThreatSonar/CompromiseAssessment.svg'/>
+          <div className={styles['content']}>
+            
+          </div>
         </div>
       </div>
       <div className={styles['article-wrap-third']}>
         <div className={styles['card']}>
           <img src='/images/ThreatSonar/IncidentResponse.svg'/>
+          <div className={styles['content']}>
+            
+          </div>
         </div>
       </div>
       <div className={styles['article-wrap-fourth']}>
         <div className={styles['card']}>
           <img src='/images/ThreatSonar/MDRplatform.svg'/>
+          <div className={styles['content']}>
+            
+          </div>
         </div>
       </div>
     </div>
