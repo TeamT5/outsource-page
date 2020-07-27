1c00ccf6c768904d7d9b2955e59482413a64f9b7
joseph2068 <windflying2068@gmail.com>
2020-07-28 07:19:19 +0800

    feat: 新增 threat-vision pagr infosection tabs 多國語系

--- src/components/pages/products/Threatvision/sections/InfoSection.jsx
+++ src/components/pages/products/Threatvision/sections/InfoSection.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import styles from './InfoSection.module.scss'
+import PropTypes from 'prop-types'
+import useTranslation from 'src/scripts/translations/useTranslation'
 
-const Tab = () => {
+const tabConfig = {
+  ce: 'solution.vision.advantage.ce-title',
+  apac: 'solution.vision.advantage.apac-title',
+}
+
+const Tab = (props) => {
+  const { t } = useTranslation(useContext)
+  const { ce, apac } = props
   const [tabIndex, setTabIndex] = useState(0)
   return (
     <div className={styles['tab-wrap']}>
-      <button onClick={() => { setTabIndex(0) }} className={[styles['tab'], tabIndex === 0 && styles['active']].join(' ')}>Customer Engagement</button>
-      <button onClick={() => { setTabIndex(1) }} className={[styles['tab'], styles['tab-long'], tabIndex === 1 && styles['active']].join(' ')}>APAC centric, World-wide Tracking</button>
+      {ce && <button onClick={() => { setTabIndex(0) }} className={[styles['tab'], tabIndex === 0 && styles['active']].join(' ')}>{t(ce)}</button>}
+      {apac && <button onClick={() => { setTabIndex(1) }} className={[styles['tab'], styles['tab-long'], tabIndex === 1 && styles['active']].join(' ')}>{t(apac)}</button>}
     </div>
   )
 }
+
+Tab.propTypes = {
+  ce: PropTypes.string,
+  apac: PropTypes.string,
+}
+
 
 const InfoSection = () => {
   return (
     <div className={styles['container']}>
       <div className={styles['tab-box']}>
-        <Tab />
+        <Tab {...tabConfig} />
       </div>
 
     </div>
