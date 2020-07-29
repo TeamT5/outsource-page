1b1a41868cc8671e1695e8ca57c03dfa1ef359f7
4N <aiko3310@hotmail.com>
2020-07-29 15:13:33 +0800

    [style] threatsonar

--- src/components/pages/products/Threatsonar/sections/ContentSection.jsx
+++ src/components/pages/products/Threatsonar/sections/ContentSection.jsx
@@ -1,80 +1,70 @@
-import React from 'react'
-import styles from './ContentSection.module.scss'
-import useTranslation from 'src/scripts/translations/useTranslation'
+import React from "react";
+import styles from "./ContentSection.module.scss";
+import useTranslation from "src/scripts/translations/useTranslation";
+import { console } from "globalthis/implementation";
 
-const functions = [
+const content = [
   {
-    articleWrap: {
-      context: 'solution.sonar.intro.context',
-      img: '/images/ThreatSonar/what_is_sonar.svg',
-      reverse: true,
-    },
+    context: "solution.sonar.intro.context",
+    img: "/images/ThreatSonar/what_is_sonar.svg",
+    reverse: true,
   },
   {
-    articleWrapSecond: {
-      title: 'solution.sonar.function.ca-title',
-      context: 'solution.sonar.function.ca-context',
-      img: '/images/ThreatSonar/CompromiseAssessment.svg',
-      reverse: false,
-    },
+    title: "solution.sonar.function.ca-title",
+    context: "solution.sonar.function.ca-context",
+    img: "/images/ThreatSonar/CompromiseAssessment.svg",
+    reverse: false,
   },
   {
-    articleWrapThird: {
-      title: 'solution.sonar.function.ir-title',
-      context: 'solution.sonar.function.ir-context',
-      img: '/images/ThreatSonar/IncidentResponse.svg',
-      reverse: true,
-    },
+    title: "solution.sonar.function.ir-title",
+    context: "solution.sonar.function.ir-context",
+    img: "/images/ThreatSonar/IncidentResponse.svg",
+    reverse: true,
   },
   {
-    articleWrapFourth: {
-      title: 'solution.sonar.function.mdr-title',
-      context: 'solution.sonar.function.mdr-context',
-      img: '/images/ThreatSonar/MDRplatform.svg',
-      reverse: false,
-    },
+    title: "solution.sonar.function.mdr-title",
+    context: "solution.sonar.function.mdr-context",
+    img: "/images/ThreatSonar/MDRplatform.svg",
+    reverse: false,
   },
-]
+];
 
 const ContentSection = () => {
-  const { t } = useTranslation()
+  const { t, locale } = useTranslation();
+  const renderClassName = (contentItem) => {
+    if (locale === "tw") {
+      if (contentItem.reverse) {
+        return `${styles["card-reverse"]} ${styles["card"]}`;
+      }
+      return styles["card"];
+    } else {
+      if (contentItem.reverse) {
+        return `${styles["card-reverse"]} ${styles["card"]} ${styles["card-en"]}`;
+      }
+      return `${styles["card"]} ${styles["card-en"]}`;
+    }
+  };
   return (
-    <div className={styles['container']}>
-      {functions
-        && functions.map((functionItem, index) => {
-          const getItem = Object.values(functionItem)[0]
-            ? Object.values(functionItem)[0]
-            : null
-
-          return (
-            <div
-              key={index}
-              className={
-                getItem.reverse
-                  ? [styles['card-reverse'], styles['card']].join(' ')
-                  : styles['card']
-              }
-            >
-              <img
-                src={
-                  getItem && getItem.img
-                    ? Object.values(functionItem)[0].img
-                    : ''
-                }
-              />
-              <div className={styles['content']}>
-                {getItem && getItem.title && (
-                  <h4 className={styles['title']} dangerouslySetInnerHTML={{ __html: t(Object.values(functionItem)[0].title) }}></h4>
-                )}
-                {getItem && getItem.context && (
-                  <p className={styles['text']} dangerouslySetInnerHTML={{ __html: t(getItem.context) }}></p>
-                )}
-              </div>
+    <div className={styles["container"]}>
+      {content.map((contentItem, index) => {
+        return (
+          <div key={index} className={renderClassName(contentItem)}>
+            <img src={contentItem.img} />
+            <div className={styles["content"]}>
+              <h4
+                className={styles["title"]}
+                dangerouslySetInnerHTML={{ __html: t(contentItem.title) }}
+              ></h4>
+              <p
+                className={styles["text"]}
+                dangerouslySetInnerHTML={{ __html: t(contentItem.context) }}
+              ></p>
             </div>
-          )
-        })}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ContentSection
+export default ContentSection;
