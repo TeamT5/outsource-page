10eb1f1b23e19c81868a5926bedc5471ccf27375
joseph2068 <windflying2068@gmail.com>
2020-07-29 16:13:36 +0800

    新增 formDemo 圖片 和調整 code rules

--- src/components/pages/products/Threatsonar/sections/BannerContent.jsx
+++ src/components/pages/products/Threatsonar/sections/BannerContent.jsx
@@ -1,44 +1,44 @@
-import React from "react";
-import styles from "./BannerContent.module.scss";
-import useTranslation from "src/scripts/translations/useTranslation";
+import React from 'react'
+import styles from './BannerContent.module.scss'
+import useTranslation from 'src/scripts/translations/useTranslation'
 
 const BannerContent = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
-    <div className={styles["container"]}>
-      <div className={styles["title-wrap"]}>
+    <div className={styles['container']}>
+      <div className={styles['title-wrap']}>
         <img
-          className={styles["title-img"]}
+          className={styles['title-img']}
           src="/images/ThreatSonar/ts_logo.svg"
         />
-        <div className={styles["title-box"]}>
-          <p className={styles["title"]}>{t("solution.sonar.title.title")}</p>
-          <div className={styles["light-height"]}></div>
+        <div className={styles['title-box']}>
+          <p className={styles['title']}>{t('solution.sonar.title.title')}</p>
+          <div className={styles['light-height']}></div>
           <p
-            className={styles["subtitle"]}
+            className={styles['subtitle']}
             dangerouslySetInnerHTML={{
-              __html: t("solution.sonar.title.context"),
+              __html: t('solution.sonar.title.context'),
             }}
           ></p>
         </div>
       </div>
-      <div className={styles["img-wrap"]}>
+      <div className={styles['img-wrap']}>
         <img
-          className={styles["left-hero"]}
+          className={styles['left-hero']}
           src="/images/ThreatSonar/sonar_hero_left.png"
         />
         <img
-          className={styles["right-hero"]}
+          className={styles['right-hero']}
           src="/images/ThreatSonar/ThreatSonar_hero_illustration.png"
         />
-        <img className={styles["mask"]} />
+        <img className={styles['mask']} />
       </div>
-      <div className={styles["bubble"]}>
+      <div className={styles['bubble']}>
         <p>Contact Us</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BannerContent;
+export default BannerContent
--- src/components/pages/products/Threatsonar/sections/ContentSection.jsx
+++ src/components/pages/products/Threatsonar/sections/ContentSection.jsx
@@ -1,70 +1,70 @@
-import React from "react";
-import styles from "./ContentSection.module.scss";
-import useTranslation from "src/scripts/translations/useTranslation";
-import { console } from "globalthis/implementation";
+import React from 'react'
+import styles from './ContentSection.module.scss'
+import useTranslation from 'src/scripts/translations/useTranslation'
+import { console } from 'globalthis/implementation'
 
 const content = [
   {
-    context: "solution.sonar.intro.context",
-    img: "/images/ThreatSonar/what_is_sonar.svg",
+    context: 'solution.sonar.intro.context',
+    img: '/images/ThreatSonar/what_is_sonar.svg',
     reverse: true,
   },
   {
-    title: "solution.sonar.function.ca-title",
-    context: "solution.sonar.function.ca-context",
-    img: "/images/ThreatSonar/CompromiseAssessment.svg",
+    title: 'solution.sonar.function.ca-title',
+    context: 'solution.sonar.function.ca-context',
+    img: '/images/ThreatSonar/CompromiseAssessment.svg',
     reverse: false,
   },
   {
-    title: "solution.sonar.function.ir-title",
-    context: "solution.sonar.function.ir-context",
-    img: "/images/ThreatSonar/IncidentResponse.svg",
+    title: 'solution.sonar.function.ir-title',
+    context: 'solution.sonar.function.ir-context',
+    img: '/images/ThreatSonar/IncidentResponse.svg',
     reverse: true,
   },
   {
-    title: "solution.sonar.function.mdr-title",
-    context: "solution.sonar.function.mdr-context",
-    img: "/images/ThreatSonar/MDRplatform.svg",
+    title: 'solution.sonar.function.mdr-title',
+    context: 'solution.sonar.function.mdr-context',
+    img: '/images/ThreatSonar/MDRplatform.svg',
     reverse: false,
   },
-];
+]
 
 const ContentSection = () => {
-  const { t, locale } = useTranslation();
+  const { t, locale } = useTranslation()
   const renderClassName = (contentItem) => {
-    if (locale === "tw") {
+    if (locale === 'tw') {
       if (contentItem.reverse) {
-        return `${styles["card-reverse"]} ${styles["card"]}`;
+        return `${styles['card-reverse']} ${styles['card']}`
       }
-      return styles["card"];
+      return styles['card']
     } else {
       if (contentItem.reverse) {
-        return `${styles["card-reverse"]} ${styles["card"]} ${styles["card-en"]}`;
+        return `${styles['card-reverse']} ${styles['card']} ${styles['card-en']}`
       }
-      return `${styles["card"]} ${styles["card-en"]}`;
+      return `${styles['card']} ${styles['card-en']}`
     }
-  };
+  }
   return (
-    <div className={styles["container"]}>
+    <div className={styles['container']}>
       {content.map((contentItem, index) => {
         return (
           <div key={index} className={renderClassName(contentItem)}>
             <img src={contentItem.img} />
-            <div className={styles["content"]}>
+            <div className={styles['content']}>
               <h4
-                className={styles["title"]}
+                className={styles['title']}
                 dangerouslySetInnerHTML={{ __html: t(contentItem.title) }}
               ></h4>
               <p
-                className={styles["text"]}
+                className={styles['text']}
                 dangerouslySetInnerHTML={{ __html: t(contentItem.context) }}
               ></p>
             </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default ContentSection;
+export default ContentSection
