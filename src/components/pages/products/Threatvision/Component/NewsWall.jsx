3e5bb38c9a739d7dd924a804e84cd1675f4920e1
4N <aiko3310@hotmail.com>
2020-08-02 16:17:26 +0800

    [add] NewsWall修正, ThreatSonar + ThreatVision 表單問題

--- src/components/pages/products/Threatvision/Component/NewsWall.jsx
+++ src/components/pages/products/Threatvision/Component/NewsWall.jsx
@@ -1,37 +1,75 @@
-import React from 'react'
-import styles from './NewsWall.module.scss'
-import useTranslation from 'src/scripts/translations/useTranslation'
+import React, { useRef, useState, useEffect, createRef } from "react";
+import styles from "./NewsWall.module.scss";
+import useTranslation from "src/scripts/translations/useTranslation";
 
 const newsWallConfig = {
-  title: 'solution.vision.newswall.title',
+  title: "solution.vision.newswall.title",
   news: [
-    'solution.vision.newswall.news-1',
-    'solution.vision.newswall.news-2',
-    'solution.vision.newswall.news-3',
-    'solution.vision.newswall.news-4',
-    'solution.vision.newswall.news-5',
-    'solution.vision.newswall.news-6',
+    "solution.vision.newswall.news-1",
+    "solution.vision.newswall.news-2",
+    "solution.vision.newswall.news-3",
+    "solution.vision.newswall.news-4",
+    "solution.vision.newswall.news-5",
+    "solution.vision.newswall.news-6",
   ],
-}
+};
 
 const NewsWall = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+  const dialogCardRefs = useRef(newsWallConfig.news.map((_, i) => createRef()));
+  const [currentScrollTop, setCurrentScrollTop] = useState({
+    offsetHeight: 0,
+    clientHeight: 0,
+    scrollTop: 0,
+  });
+
+  const getScrollTop = (e) => {
+    const { offsetHeight, clientHeight, scrollTop } = e.target.scrollingElement;
+    if (currentScrollTop.scrollTop === scrollTop) {
+      return;
+    }
+    setCurrentScrollTop({ offsetHeight, clientHeight, scrollTop });
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", getScrollTop);
+    return () => {
+      document.removeEventListener("scroll", getScrollTop);
+    };
+  }, []);
+  const renderDialogShowClassName = (i) => {
+    if (
+      dialogCardRefs.current[i] &&
+      dialogCardRefs.current[i].current &&
+      !dialogCardRefs.current[i].current.isShow &&
+      currentScrollTop.scrollTop -
+        currentScrollTop.clientHeight -
+        dialogCardRefs.current[i].current.getBoundingClientRect().top >=
+        dialogCardRefs.current[i].current.getBoundingClientRect().top
+    ) {
+      return styles["show"];
+    }
+    return "";
+  };
   return (
-    <div className={styles['container']}>
+    <div className={styles["container"]}>
       <h3
-        className={[styles['title']]}
+        className={[styles["title"]]}
         dangerouslySetInnerHTML={{ __html: `${t(newsWallConfig.title)}` }}
       />
-      {newsWallConfig.news.map((dialog, index) => (
+      {dialogCardRefs.current.map((_, index) => (
         <div
           key={index}
-          className={[styles['dialog']]}
-          dangerouslySetInnerHTML={{ __html: `${t(dialog)}` }}
+          className={`${styles["dialog"]} ${renderDialogShowClassName(index)}`}
+          ref={dialogCardRefs.current[index]}
+          dangerouslySetInnerHTML={{
+            __html: `${t(newsWallConfig.news[index])}`,
+          }}
         />
       ))}
-      <div className={[styles['bg-arrow']]} />
+      <div className={[styles["bg-arrow"]]} />
     </div>
-  )
-}
+  );
+};
 
-export default NewsWall
+export default NewsWall;
--- src/components/pages/products/Threatvision/sections/InfoSection.jsx
+++ src/components/pages/products/Threatvision/sections/InfoSection.jsx
@@ -1,160 +1,160 @@
-import React, { useState, useContext } from 'react'
-import styles from './InfoSection.module.scss'
-import useTranslation from 'src/scripts/translations/useTranslation'
+import React, { useState, useContext } from "react";
+import styles from "./InfoSection.module.scss";
+import useTranslation from "src/scripts/translations/useTranslation";
 import {
   NewsWall,
   CategoryCard,
-} from 'components/pages/products/Threatvision/Component'
-import { XGrid } from '../../../../common/ui-components'
+} from "components/pages/products/Threatvision/Component";
+import { XGrid } from "../../../../common/ui-components";
 
 const tabConfig = {
   ce: {
-    title: 'solution.vision.advantage.ce-title',
-    context: 'solution.vision.advantage.ce-context',
+    title: "solution.vision.advantage.ce-title",
+    context: "solution.vision.advantage.ce-context",
   },
   apac: {
-    title: 'solution.vision.advantage.apac-title',
-    context: 'solution.vision.advantage.apac-context',
+    title: "solution.vision.advantage.apac-title",
+    context: "solution.vision.advantage.apac-context",
   },
-}
+};
 
 const imagesConfig = {
   ce: [
     {
-      title: 'solution.vision.advantage.ce-trait-1',
-      img: '/images/ThreatVision/Customer Engagement_online_sandbox.svg',
+      title: "solution.vision.advantage.ce-trait-1",
+      img: "/images/ThreatVision/Customer Engagement_online_sandbox.svg",
     },
     {
-      title: 'solution.vision.advantage.ce-trait-2',
-      img: '/images/ThreatVision/Customer Engagement_Keyword_search.svg',
+      title: "solution.vision.advantage.ce-trait-2",
+      img: "/images/ThreatVision/Customer Engagement_Keyword_search.svg",
     },
     {
-      title: 'solution.vision.advantage.ce-trait-3',
-      img: '/images/ThreatVision/Customer Engagement_Technical_data.svg',
+      title: "solution.vision.advantage.ce-trait-3",
+      img: "/images/ThreatVision/Customer Engagement_Technical_data.svg",
     },
     {
-      title: 'solution.vision.advantage.ce-trait-4',
-      img: '/images/ThreatVision/Customer Engagement_API.svg',
+      title: "solution.vision.advantage.ce-trait-4",
+      img: "/images/ThreatVision/Customer Engagement_API.svg",
     },
   ],
   apac: [
     {
-      title: 'solution.vision.advantage.apac-trait-1',
-      img: '/images/ThreatVision/APACcentric_samples.svg',
+      title: "solution.vision.advantage.apac-trait-1",
+      img: "/images/ThreatVision/APACcentric_samples.svg",
     },
     {
-      title: 'solution.vision.advantage.apac-trait-2',
-      img: '/images/ThreatVision/APACcentric_malware.svg',
+      title: "solution.vision.advantage.apac-trait-2",
+      img: "/images/ThreatVision/APACcentric_malware.svg",
     },
     {
-      title: 'solution.vision.advantage.apac-trait-3',
-      img: '/images/ThreatVision/APACcentric_Adversary.svg',
+      title: "solution.vision.advantage.apac-trait-3",
+      img: "/images/ThreatVision/APACcentric_Adversary.svg",
     },
   ],
-}
+};
 
 const Tab = () => {
-  const { t } = useTranslation()
-  const [tabIndex, setTabIndex] = useState(0)
+  const { t } = useTranslation();
+  const [tabIndex, setTabIndex] = useState(0);
   const renderTabContent = () => {
     if (tabIndex === 0) {
       return (
-        <div className={styles['tab-content-ce']}>
+        <div className={styles["tab-content-ce"]}>
           <p
-            className={styles['title']}
+            className={styles["title"]}
             dangerouslySetInnerHTML={{ __html: `${t(tabConfig.ce.context)}` }}
           />
-          <div className={styles['context']}>
+          <div className={styles["context"]}>
             {imagesConfig.ce.map((card, index) => {
               return (
-                <div className={styles['img-box']} key={index}>
+                <div className={styles["img-box"]} key={index}>
                   <img src={card.img} />
                   <p
-                    className={styles['card-title']}
+                    className={styles["card-title"]}
                     dangerouslySetInnerHTML={{ __html: `${t(card.title)}` }}
                   />
                 </div>
-              )
+              );
             })}
           </div>
         </div>
-      )
+      );
     }
     return (
-      <div className={styles['tab-content-apac']}>
+      <div className={`${styles["tab-content-apac"]}`}>
         <p
-          className={styles['title']}
+          className={styles["title"]}
           dangerouslySetInnerHTML={{
             __html: `${t(tabConfig.apac.context)}`,
           }}
         />
-        <div className={styles['context']}>
+        <div className={styles["context"]}>
           {imagesConfig.apac.map((card, index) => {
             return (
-              <div className={styles['img-box']} key={index}>
+              <div className={styles["img-box"]} key={index}>
                 <img src={card.img} />
                 <p
-                  className={styles['card-title']}
+                  className={styles["card-title"]}
                   dangerouslySetInnerHTML={{ __html: `${t(card.title)}` }}
                 />
               </div>
-            )
+            );
           })}
         </div>
       </div>
-    )
-  }
+    );
+  };
   return (
-    <div className={styles['tab-container']}>
-      <div className={styles['tab-wrap']}>
+    <div className={styles["tab-container"]}>
+      <div className={styles["tab-wrap"]}>
         <button
           dangerouslySetInnerHTML={{ __html: `${t(tabConfig.ce.title)}` }}
           onClick={() => {
-            setTabIndex(0)
+            setTabIndex(0);
           }}
           className={
             tabIndex === 0
-              ? `${styles['tab']} ${styles['active']}`
-              : `${styles['tab']}`
+              ? `${styles["tab"]} ${styles["active"]}`
+              : `${styles["tab"]}`
           }
         />
         <button
           dangerouslySetInnerHTML={{ __html: `${t(tabConfig.apac.title)}` }}
           onClick={() => {
-            setTabIndex(1)
+            setTabIndex(1);
           }}
           className={
             tabIndex === 1
-              ? `${styles['tab']} ${styles['active']}`
-              : `${styles['tab']}`
+              ? `${styles["tab"]} ${styles["active"]}`
+              : `${styles["tab"]}`
           }
         />
       </div>
       {renderTabContent()}
     </div>
-  )
-}
+  );
+};
 
 const InfoSection = () => {
   return (
-    <div className={styles['container']}>
-      <div className={styles['tab-box']}>
+    <div className={styles["container"]}>
+      <div className={styles["tab-box"]}>
         <Tab />
       </div>
-      <div className={styles['newswall-container']}>
+      <div className={styles["newswall-container"]}>
         <NewsWall />
       </div>
-      <div className={styles['category-container']}>
+      <div className={styles["category-container"]}>
         <CategoryCard />
       </div>
-      <div className={styles['xgrid-container-left']}>
+      <div className={styles["xgrid-container-left"]}>
         <XGrid type="dark" />
       </div>
-      <div className={styles['xgrid-container-right']}>
+      <div className={styles["xgrid-container-right"]}>
         <XGrid type="dark" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InfoSection
+export default InfoSection;
